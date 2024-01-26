30132d59fedcd6b52968fdc40e1d9ee8ee11e0c3
Jean Carlos <jean@naxostelecom.com.br>
2024-01-26 13:38:17 -0300

    criado menu

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nome = "Nome de Exemplo";
@@ -9,5 +11,23 @@ public class Desafio {
         System.out.println("Tipo da Conta: " + tipoDeConta);
         System.out.println("Salto Atual: " + saldo);
         System.out.println("****************************************");
+
+        String menu = """
+                \n########################################
+                *** Digite sua opção ***
+                1 - Consultar Saldo
+                2 - Sacar Valor
+                3 - Depositar Valor
+                4 - SAIR
+                ########################################
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        int opcao = 0;
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
     }
 }
