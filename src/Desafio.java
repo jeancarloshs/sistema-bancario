2a8f8b5ac6dc2ff8deecf4793e9061d9f5f1faf3
Jean Carlos <jean@naxostelecom.com.br>
2024-01-26 10:51:54 -0300

    first commit

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Nome de Exemplo";
+        String tipoDeConta = "Conta Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("****************************************");
+        System.out.println("Nome do Cliente: " + nome);
+        System.out.println("Tipo da Conta: " + tipoDeConta);
+        System.out.println("Salto Atual: " + saldo);
+        System.out.println("****************************************");
+    }
+}
