e7dc515c3ed38c651540dea9e2cbebe1dbe08c75
Jean Carlos <jean@naxostelecom.com.br>
2024-02-03 10:32:28 -0300

    finalizado sistema bancario

--- src/Desafio.java
+++ src/Desafio.java
@@ -28,6 +28,32 @@ public class Desafio {
         while (opcao != 4) {
             System.out.println(menu);
             opcao = leitura.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("Saldo Atual: " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja Transferir?");
+                double valorSaida = leitura.nextDouble();
+                if (valorSaida > saldo) {
+                    System.out.println("Saldo insuficiente!!!");
+                } else {
+                    saldo = saldo - valorSaida;
+                    System.out.println("Saldo Atualizado: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Qual o valor que deseja Depositar?");
+                double valorEntrada = leitura.nextDouble();
+                if (valorEntrada <= 0) {
+                    System.out.println("Valor incorreto!!!");
+                } else {
+                    saldo = saldo + valorEntrada;
+                    System.out.println("Saldo Atualizado: " + saldo);
+                }
+            } else if (opcao == 4) {
+                System.out.println("Programa Finalizado");
+            } else if (opcao != 4) {
+                System.out.println("Opção Invalida");
+            }
         }
     }
 }
